75647a78c16f7963003dd19e17275fcbe50aa83c
Radek Novotný <novotnyradekcz@gmail.com>
2023-01-26 20:09:47 +0100

    Stage 1

---
+++ src/main/kotlin/Stage1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val ranks = listOf<String>("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+    val suits = listOf<String>("♦", "♥", "♠", "♣")
+    for (rank in ranks) print("$rank ")
+    println()
+    for (suit in suits) print("$suit ")
+    println()
+    for (suit in suits) {
+        for (rank in ranks) {
+            print("$rank$suit ")
+        }
+    }
+}
